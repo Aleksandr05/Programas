69e8e77383822487873add0ce322aa38cf170640
Aleksandr05 <63678663+Aleksandr05@users.noreply.github.com>
2020-04-14 18:36:34 -0300

    upload dos execícios

---
+++ Calculadora.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+		int n1;
+		int n2;
+		
+		try (Scanner input = new Scanner(System.in)) {
+			System.out.println("Qual é o primeiro número?");
+			n1 = input.nextInt();
+			System.out.println("Qual o segundo número?");
+			n2 = input.nextInt();
+		}
+		
+		System.out.println("Soma: "+(n1+n2));
+		System.out.println("Subtração: "+(n1-n2));
+		System.out.println("Multiplicação: "+(n1*n2));
+		System.out.println("Divisão inteira: "+(n1/n2));
+		System.out.println("Módulo: "+(n1%n2));
+
+		
+		
+		
+	}
+
+}
